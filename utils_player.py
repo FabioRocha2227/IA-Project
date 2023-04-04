1f69039f4037321a8ef1a9bbeec5eaf6c7b19193
TMBMarques <48417326+TMBMarques@users.noreply.github.com>
2023-04-04 02:28:35 +0100

    improved atom selection

--- utils_player.py
+++ utils_player.py
@@ -2,178 +2,105 @@
 def select_atom_up(level):
     selected_y = level.atoms_list[level.selected_atom].y
 
-    nearest_y = 0
-
+    atoms_up = []
     for i in range(len(level.atoms_list)):
         if i != level.selected_atom and level.atoms_list[i].y < selected_y:
-            if level.atoms_list[i].y > nearest_y:
-                nearest_y = level.atoms_list[i].y
-                level.selected_atom = i
+            atoms_up.append(i)
+
+    nearest_x = len(level.matrix[0])
+    atoms_nearest_x = []
+
+    for i in range(len(atoms_up)):
+        if abs(level.atoms_list[level.selected_atom].x - level.atoms_list[atoms_up[i]].x) < nearest_x:
+            atoms_nearest_x = [atoms_up[i]]
+            nearest_x = abs(level.atoms_list[level.selected_atom].x - level.atoms_list[atoms_up[i]].x)
+        elif abs(level.atoms_list[level.selected_atom].x - level.atoms_list[atoms_up[i]].x) < nearest_x:
+            atoms_nearest_x.append(atoms_up[i])
+
+    nearest_y = 0
+
+    for i in range(len(atoms_nearest_x)):
+        if level.atoms_list[atoms_nearest_x[i]].y > nearest_y:
+                nearest_y = level.atoms_list[atoms_nearest_x[i]].y
+                level.selected_atom = atoms_nearest_x[i]
+
+
 
 def select_atom_down(level):
     selected_y = level.atoms_list[level.selected_atom].y
 
-    nearest_y = len(level.matrix)
-
+    atoms_down = []
     for i in range(len(level.atoms_list)):
         if i != level.selected_atom and level.atoms_list[i].y > selected_y:
-            if level.atoms_list[i].y < nearest_y:
-                nearest_y = level.atoms_list[i].y
-                level.selected_atom = i
+            atoms_down.append(i)
+
+    nearest_x = len(level.matrix[0])
+    atoms_nearest_x = []
+
+    for i in range(len(atoms_down)):
+        if abs(level.atoms_list[level.selected_atom].x - level.atoms_list[atoms_down[i]].x) < nearest_x:
+            atoms_nearest_x = [atoms_down[i]]
+            nearest_x = abs(level.atoms_list[level.selected_atom].x - level.atoms_list[atoms_down[i]].x)
+        elif abs(level.atoms_list[level.selected_atom].x - level.atoms_list[atoms_down[i]].x) < nearest_x:
+            atoms_nearest_x.append(atoms_down[i])
+
+    nearest_y = len(level.matrix)
+
+    for i in range(len(atoms_nearest_x)):
+        if level.atoms_list[atoms_nearest_x[i]].y < nearest_y:
+                nearest_y = level.atoms_list[atoms_nearest_x[i]].y
+                level.selected_atom = atoms_nearest_x[i]
+
+
 
 def select_atom_left(level):
     selected_x = level.atoms_list[level.selected_atom].x
+
+    atoms_left = []
+    for i in range(len(level.atoms_list)):
+        if i != level.selected_atom and level.atoms_list[i].x < selected_x:
+            atoms_left.append(i)
+
+    nearest_y = len(level.matrix)
+    atoms_nearest_y = []
+
+    for i in range(len(atoms_left)):
+        if abs(level.atoms_list[level.selected_atom].y - level.atoms_list[atoms_left[i]].y) < nearest_y:
+            atoms_nearest_y = [atoms_left[i]]
+            nearest_y = abs(level.atoms_list[level.selected_atom].y - level.atoms_list[atoms_left[i]].y)
+        elif abs(level.atoms_list[level.selected_atom].y - level.atoms_list[atoms_left[i]].y) < nearest_y:
+            atoms_nearest_y.append(atoms_left[i])
 
     nearest_x = 0
 
-    for i in range(len(level.atoms_list)):
-        if i != level.selected_atom and level.atoms_list[i].x < selected_x:
-            if level.atoms_list[i].x > nearest_x:
-                nearest_x = level.atoms_list[i].x
-                level.selected_atom = i
+    for i in range(len(atoms_nearest_y)):
+        if level.atoms_list[atoms_nearest_y[i]].x > nearest_x:
+                nearest_x = level.atoms_list[atoms_nearest_y[i]].x
+                level.selected_atom = atoms_nearest_y[i]
+
+
 
 def select_atom_right(level):
     selected_x = level.atoms_list[level.selected_atom].x
 
-    nearest_x = len(level.matrix[0])
-
+    atoms_right = []
     for i in range(len(level.atoms_list)):
         if i != level.selected_atom and level.atoms_list[i].x > selected_x:
-            if level.atoms_list[i].x < nearest_x:
-                nearest_x = level.atoms_list[i].x
-                level.selected_atom = i
+            atoms_right.append(i)
 
+    nearest_y = len(level.matrix)
+    atoms_nearest_y = []
 
+    for i in range(len(atoms_right)):
+        if abs(level.atoms_list[level.selected_atom].y - level.atoms_list[atoms_right[i]].y) < nearest_y:
+            atoms_nearest_y = [atoms_right[i]]
+            nearest_y = abs(level.atoms_list[level.selected_atom].y - level.atoms_list[atoms_right[i]].y)
+        elif abs(level.atoms_list[level.selected_atom].y - level.atoms_list[atoms_right[i]].y) < nearest_y:
+            atoms_nearest_y.append(atoms_right[i])
 
+    nearest_x = len(level.matrix[0])
 
-""" import numpy as np
-
-
-
-def select_atom_up(level):
-    selected_atom_index = np.where(level.state == level.selected_atom)
-    selected_atom_x, selected_atom_y = selected_atom_index[1][0], selected_atom_index[0][0]
-
-    sides = 2
-
-    if selected_atom_y > 1:
-        for x in range(0, -len(level.state[0]), -1):
-            x_offset = x
-
-            if selected_atom_x + x_offset < 1 and x_offset != 0 and sides == 2:
-                x_offset = abs(x_offset)
-                sides = 1
-
-            for i in range(0, sides):
-                if i == 1:
-                    x_offset = abs(x_offset)
-
-                if x_offset >= 0 and selected_atom_x + x_offset > len(level.state[0]) - 2:
-                    sides = 1
-                    break
-
-                for y in range(selected_atom_y - 1, 0, -1):
-                    if level.state[y][selected_atom_x + x_offset] > 0:
-                        level.selected_atom = level.state[y][selected_atom_x + x_offset]
-                        return
-                    
-                if x_offset == 0:
-                    break
-
-
-
-def select_atom_down(level):
-    selected_atom_index = np.where(level.state == level.selected_atom)
-    selected_atom_x, selected_atom_y = selected_atom_index[1][0], selected_atom_index[0][0]
-
-    x_offset = 0
-    sides = 2
-
-    if selected_atom_y < len(level.state) - 2:
-        for x in range(0, -len(level.state[0]), -1):
-            x_offset = x
-
-            if selected_atom_x + x_offset < 1 and x_offset != 0 and sides == 2:
-                x_offset = abs(x_offset)
-                sides = 1
-
-            for i in range(0, sides):
-                if i == 1:
-                    x_offset = abs(x_offset)
-
-                if x_offset >= 0 and selected_atom_x + x_offset > len(level.state[0]) - 2:
-                    sides = 1
-                    break
-
-                for y in range(selected_atom_y + 1, len(level.state)):
-                    if level.state[y][selected_atom_x + x_offset] > 0:
-                        level.selected_atom = level.state[y][selected_atom_x + x_offset]
-                        return
-                    
-                if x_offset == 0:
-                    break
-
-
-
-def select_atom_left(level):
-    selected_atom_index = np.where(level.state == level.selected_atom)
-    selected_atom_x, selected_atom_y = selected_atom_index[1][0], selected_atom_index[0][0]
-
-    sides = 2
-
-    if selected_atom_x > 1:
-        for y in range(0, -len(level.state), -1):
-            y_offset = y
-
-            if selected_atom_y + y_offset < 1 and y_offset != 0 and sides == 2:
-                y_offset = abs(y_offset)
-                sides = 1
-
-            for i in range(0, sides):
-                if i == 1:
-                    y_offset = abs(y_offset)
-
-                if y_offset >= 0 and selected_atom_y + y_offset > len(level.state) - 2:
-                    sides = 1
-                    break
-
-                for x in range(selected_atom_x - 1, 0, -1):
-                    if level.state[selected_atom_y + y_offset][x] > 0:
-                        level.selected_atom = level.state[selected_atom_y + y_offset][x]
-                        return
-                    
-                if y_offset == 0:
-                    break
-
-
-
-def select_atom_right(level):
-    selected_atom_index = np.where(level.state == level.selected_atom)
-    selected_atom_x, selected_atom_y = selected_atom_index[1][0], selected_atom_index[0][0]
-
-    sides = 2
-
-    if selected_atom_x < len(level.state[0]) - 2:
-        for y in range(0, -len(level.state), -1):
-            y_offset = y
-
-            if selected_atom_y + y_offset < 1 and y_offset != 0 and sides == 2:
-                y_offset = abs(y_offset)
-                sides = 1
-
-            for i in range(0, sides):
-                if i == 1:
-                    y_offset = abs(y_offset)
-
-                if y_offset >= 0 and selected_atom_y + y_offset > len(level.state) - 2:
-                    sides = 1
-                    break
-                
-                for x in range(selected_atom_x + 1, len(level.state[0])):   
-                    if level.state[selected_atom_y + y_offset][x] > 0:
-                        level.selected_atom = level.state[selected_atom_y + y_offset][x]
-                        return
-                    
-                if y_offset == 0:
-                    break
- """
+    for i in range(len(atoms_nearest_y)):
+        if level.atoms_list[atoms_nearest_y[i]].x < nearest_x:
+                nearest_x = level.atoms_list[atoms_nearest_y[i]].x
+                level.selected_atom = atoms_nearest_y[i]
